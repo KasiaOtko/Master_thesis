4facaf0ebd663f2a68d0a6b6230e2083eae06105
KasiaOtko <katin010@gmail.com>
2022-03-13 16:11:22 +0100

    Initial commit with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code and data for master thesis project "Graph Neural Networks for social systems"',
+    author='Katarzyna Otko',
+    license='MIT',
+)
