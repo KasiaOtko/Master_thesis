8f50c95150740d9bb870cf787ae94666b378cd65
KasiaOtko <katin010@gmail.com>
2022-03-16 17:04:40 +0100

    Pep8

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,13 +4,13 @@ from ogb.nodeproppred import PygNodePropPredDataset
 
 
 def load_data(dataset_name: str, root: str) -> torch_geometric.data.Data:
-    """ 
-        - dataset_name (str) - name of the dataset to load
-        - root (str) - path to thr root of the folder storing the dataset
+    """
+    - dataset_name (str) - name of the dataset to load
+    - root (str) - path to thr root of the folder storing the dataset
     """
 
-    if 'ogb' in dataset_name:
-        dataset = PygNodePropPredDataset(name = dataset_name, root = root)
+    if "ogb" in dataset_name:
+        dataset = PygNodePropPredDataset(name=dataset_name, root=root)
     else:
         dataset = None
 
--- src/models/ml_classifiers/train_model.py
+++ src/models/ml_classifiers/train_model.py
@@ -14,20 +14,26 @@ from src.data.make_dataset import load_data
 wandb.init(project="master-thesis")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("dataset_name", choices=['ogbn-products', 'ogbn-arxiv', 'EU-judgements'],
-                    help = "Name of the dataset to use (possible: 'ogbn-products', 'ogbn-arxiv', 'EU-judgements')")
-parser.add_argument("--root", default = 'data/raw',
-                    help="Root directory of a folder storing OGB dataset")
-parser.add_argument("--model", choices = ["lr", "rf", "xgb"],
-                    help="Model to tune")
+parser.add_argument(
+    "dataset_name",
+    choices=["ogbn-products", "ogbn-arxiv", "EU-judgements"],
+    help="Name of the dataset to use (possible: 'ogbn-products', 'ogbn-arxiv', 'EU-judgements')",
+)
+parser.add_argument(
+    "--root", default="data/raw", help="Root directory of a folder storing OGB dataset"
+)
+parser.add_argument("--model", choices=["lr", "rf", "xgb"], help="Model to tune")
 
-logging.basicConfig(filename="logs/ml_classifiers.txt",
-                    filemode='a',
-                    level=logging.INFO, 
-                    format='%(asctime)s %(levelname)-8s %(message)s', 
-                    datefmt='%Y-%m-%d %H:%M:%S')                 
+logging.basicConfig(
+    filename="logs/ml_classifiers.txt",
+    filemode="a",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
-#args = parser.parse_args()
+# args = parser.parse_args()
+
 
 def data_split(data):
 
@@ -38,18 +44,19 @@ def data_split(data):
     for key, idx in split_idx.items():
         mask = torch.zeros(data.num_nodes, dtype=torch.bool)
         mask[idx] = True
-        data[f'{key}_mask'] = mask
+        data[f"{key}_mask"] = mask
 
-    X_train = data.x[data['train_mask']].numpy()
-    y_train = data.y[data['train_mask']].numpy().ravel()
-    X_valid = data.x[data['valid_mask']].numpy()
-    y_valid = data.y[data['valid_mask']].numpy().ravel()
-    X_test = data.x[data['test_mask']].numpy()
-    y_test = data.y[data['test_mask']].numpy().ravel()
+    X_train = data.x[data["train_mask"]].numpy()
+    y_train = data.y[data["train_mask"]].numpy().ravel()
+    X_valid = data.x[data["valid_mask"]].numpy()
+    y_valid = data.y[data["valid_mask"]].numpy().ravel()
+    X_test = data.x[data["test_mask"]].numpy()
+    y_test = data.y[data["test_mask"]].numpy().ravel()
 
     return X_train, y_train, X_valid, y_valid, X_test, y_test
 
-def LogReg(data, scale = True):
+
+def LogReg(data, scale=True):
 
     X_train, y_train, X_valid, y_valid, X_test, y_test = data_split(data)
 
@@ -60,38 +67,53 @@ def LogReg(data, scale = True):
         X_valid = scaler.transform(X_valid)
         X_test = scaler.transform(X_test)
 
-    C_range = [10, 100] #[0.01,0.1,1,10,100, 100]
-    acc_table = pd.DataFrame(columns = ['Train_accuracy', 'Valid_accuracy', 'Test accuracy'], index = [0.001,0.01,0.1,1,10,100])
-    #acc_table["C_parameter"] = C_range
+    C_range = [10, 100]  # [0.01,0.1,1,10,100, 100]
+    acc_table = pd.DataFrame(
+        columns=["Train_accuracy", "Valid_accuracy", "Test accuracy"],
+        index=[0.001, 0.01, 0.1, 1, 10, 100],
+    )
+    # acc_table["C_parameter"] = C_range
 
     for C in C_range:
-        lr = LogisticRegression(random_state=0, C = C, max_iter = 300).fit(X_train, y_train)
+        lr = LogisticRegression(random_state=0, C=C, max_iter=300).fit(X_train, y_train)
         y_train_pred = lr.predict(X_train)
         y_valid_pred = lr.predict(X_valid)
-        acc_table.loc[C, "Train_accuracy"] = pd.to_numeric(accuracy_score(y_train, y_train_pred))
-        acc_table.loc[C, "Valid_accuracy"] = pd.to_numeric(accuracy_score(y_valid, y_valid_pred))
-        logging.info('C = %d: Train_accuracy = %f, Validation accuracy = %f', 
-                        C, acc_table.loc[C, "Train_accuracy"], acc_table.loc[C, "Valid_accuracy"])
+        acc_table.loc[C, "Train_accuracy"] = pd.to_numeric(
+            accuracy_score(y_train, y_train_pred)
+        )
+        acc_table.loc[C, "Valid_accuracy"] = pd.to_numeric(
+            accuracy_score(y_valid, y_valid_pred)
+        )
+        logging.info(
+            "C = %d: Train_accuracy = %f, Validation accuracy = %f",
+            C,
+            acc_table.loc[C, "Train_accuracy"],
+            acc_table.loc[C, "Valid_accuracy"],
+        )
 
     wandb_table = wandb.Table(dataframe=acc_table)
-    wandb.log({'acc_table': wandb_table})
+    wandb.log({"acc_table": wandb_table})
 
-    C_final = pd.to_numeric(acc_table[~acc_table["Valid_accuracy"].isna()]["Valid_accuracy"]).idxmax()
-    lr = LogisticRegression(random_state=0, C = int(C_final), max_iter = 300).fit(X_train, y_train)
+    C_final = pd.to_numeric(
+        acc_table[~acc_table["Valid_accuracy"].isna()]["Valid_accuracy"]
+    ).idxmax()
+    lr = LogisticRegression(random_state=0, C=int(C_final), max_iter=300)
+    lr.fit(X_train, y_train)
     y_test_pred = lr.predict(X_test)
     test_accuracy = accuracy_score(y_test, y_test_pred)
-    wandb.log({'test_accuracy': test_accuracy})
-    
+    wandb.log({"test_accuracy": test_accuracy})
+
     # maybe consider saving the df here
 
     return acc_table, test_accuracy, lr
 
+
 if __name__ == "__main__":
-    logging.info('Start.')
-    #print(args.root)
-    #print(args.dataset_name)
-    
-    #data = load_data(args.dataset_name, args.root)
-    data = load_data('ogbn-arxiv', 'data/raw')
-    logging.info('Data loaded.')
+    logging.info("Start.")
+    # print(args.root)
+    # print(args.dataset_name)
+
+    # data = load_data(args.dataset_name, args.root)
+    data = load_data("ogbn-arxiv", "data/raw")
+    logging.info("Data loaded.")
     acc_table, final_score, best_model = LogReg(data)
