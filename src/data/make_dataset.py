89f2dd7f58cd218e50884013bb7ec253340549b1
KasiaOtko <katin010@gmail.com>
2022-03-16 14:32:56 +0100

    make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,29 +2,32 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import argparse
+from ogb.nodeproppred import PygNodePropPredDataset
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-root", "--root-ogb-dataset", default = 'data/raw',
+                    help="Root directory of a folder storing OGB dataset")
+parser.add_argument("-d", "--dataset_name",
+                    help = "Name of the dataset to use (possible: 'ogbn-products', 'ogbn-arxiv', 'EU-judgements')")
+args = parser.parse_args()
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def main(dataset_name):
+    """ 
+        - dataset_nameReturn dataset
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Making final data set from raw data')
 
+    if 'ogb' in args.d:
+        dataset = PygNodePropPredDataset(name = dataset_name, root = args.root)
+    else:
+        dataset = None
+
+    return dataset
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    data = main('ogbn-arxiv')
